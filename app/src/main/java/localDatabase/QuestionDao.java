b7e6c02fecad483bf58f97698d013559a9000fd3
MattiaColombari <mattia.colombari00@gmail.com>
2021-05-13 19:59:17 +0200

    Managed "EmptyResultSetException" and Error: "Only the original thread that created a view hierarchy can touch its views." with "runOnUiThread" (Line 59 localDatabase/TestApp.java).

--- app/src/main/java/localDatabase/QuestionDao.java
+++ app/src/main/java/localDatabase/QuestionDao.java
@@ -2,6 +2,7 @@ package localDatabase;
 
 import androidx.room.Dao;
 import androidx.room.Delete;
+import androidx.room.EmptyResultSetException;
 import androidx.room.Insert;
 import androidx.room.Query;
 
@@ -10,13 +11,13 @@ import java.util.List;
 @Dao
 public interface QuestionDao {
     @Query("SELECT * FROM question")
-    List<Question> getAll();
+    List<Question> getAll() throws EmptyResultSetException;
 
     @Query("SELECT * FROM question WHERE qid IN (:qIds)")
-    List<Question> loadAllByIds(int[] qIds);
+    List<Question> loadAllByIds(int[] qIds) throws EmptyResultSetException;
 
     @Query("SELECT * FROM question WHERE questionText IN (:values)")
-    List<Question> loadAllByValue(int[] values);
+    List<Question> loadAllByValue(int[] values) throws EmptyResultSetException;
 
     @Insert
     void insertAll(Question... question);
--- app/src/main/java/localDatabase/TestApp.java
+++ app/src/main/java/localDatabase/TestApp.java
@@ -2,6 +2,7 @@ package localDatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Dao;
+import androidx.room.EmptyResultSetException;
 import androidx.room.Room;
 import android.content.Context;
 import android.widget.TextView;
@@ -14,31 +15,52 @@ import java.util.List;
 public class TestApp implements Runnable{
     QuestionDao qDao;
     QuestionDatabase db;
+    Home home;
     TextView mainTextView;
-    public TestApp(Context context, Home T) {
+    public TestApp(Context context, Home home, TextView mainTextView) {
         db = Room.databaseBuilder(context, QuestionDatabase.class, "Questions").build();
         qDao = db.questionDao();
-        this.mainTextView = T.findViewById(R.id.mainText);
+        this.home = home;
+        this.mainTextView = mainTextView;
     }
 
     public void run(){
-        //qDao.insertAll(new Question(1, "Come ti chiami???"));
+        //qDao.insertAll(new Question(1, "Come ti chiami?"));
         //qDao.deleteAll();
-        List<Question> q = qDao.getAll();
 
+        List<Question> q = null;
         StringBuilder val = new StringBuilder();
 
-        if(q.isEmpty()) {
-            val.append("Non sono presenti elementi");
+        val.append("Ritorno:");
+        val.append('\n');
+
+        /* Room select query can throw "EmptyResultSetException" when the return is empty */
+        try {
+            q = qDao.getAll();
+        }
+        catch(EmptyResultSetException e){}
+
+        if(q.isEmpty()){
+            val.append("Non sono presenti elementi.");
             val.append('\n');
         }
-        else {
-            for (Question i : q) {
-                val.append(i.toString());
-                val.append('\n');
-            }
+
+        for (Question i : q) {
+            val.append(i.toString());
+            val.append('\n');
         }
 
-        mainTextView.setText(val.toString());
+        /* The interrogation of the local db needs to be in a different Thread than the main one,
+        *  but is not possible to change element of a View in a different thread than the view it
+        *  self, for this reason i used "runOnUiThread" on the View than contains the element that i
+        *  want change, it runs the specified action (change the element) on the UI thread (home).
+        *  The action is posted to the event queue of the UI thread.
+        * */
+        home.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mainTextView.setText(val.toString());
+            }
+        });
     }
 }
