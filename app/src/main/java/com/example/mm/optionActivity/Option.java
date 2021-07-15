99902989ba669f6273fd6bc6e44dba169309d013
Alboni Matteo <270982@studenti.unimore.it>
2021-07-15 15:52:24 +0200

    Prova per aggiungere attività per Option

---
+++ app/src/main/java/com/example/mm/optionActivity/Option.java
@@ -0,0 +1,16 @@
+package com.example.mm.optionActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mm.R;
+
+public class Option extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option);
+    }
+}
