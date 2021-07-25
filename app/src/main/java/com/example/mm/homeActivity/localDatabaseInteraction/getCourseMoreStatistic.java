0257bc90fa8a158415cf5479c02096e775842f66
MattiaColombari <mattia.colombari00@gmail.com>
2021-07-25 18:32:04 +0200

    New Chart for MoreStatisticFragment.java (from LineChart to BarChart), implements that only one recyclerview row is Expanded at the time, and sort in terms of average on single date.

--- app/src/main/java/com/example/mm/homeActivity/localDatabaseInteraction/getCourseMoreStatistic.java
+++ app/src/main/java/com/example/mm/homeActivity/localDatabaseInteraction/getCourseMoreStatistic.java
@@ -53,26 +53,43 @@ public class getCourseMoreStatistic implements Runnable{
             }
             for (Course c : coursesList){
                 List<StatisticUser> statisticUsers = localDatabaseDao.getAllStatisticUserByIdCourse(c.getId());
-                ArrayList<RecyclerViewRowRecordData> recordDataArrayList = new ArrayList<>();
+                ArrayList<RecyclerViewRowRecordData> recordDataArrayListTmp = new ArrayList<>();
+                ArrayList<RecyclerViewRowRecordData> recordDataArrayListFinal = new ArrayList<>();
                 float value = 0;
                 float lastValue = 0;
                 if(!statisticUsers.isEmpty()) {
                     Collections.sort(statisticUsers);
                     for (StatisticUser s : statisticUsers) {
-                        recordDataArrayList.add(new RecyclerViewRowRecordData(s.getPoints(), s.getDate()));
+                        recordDataArrayListTmp.add(new RecyclerViewRowRecordData(s.getPoints(), s.getDate()));
                         value += s.getPoints();
                         lastValue = s.getPoints();
                     }
                     value /= statisticUsers.size();
+
+                    int prevDate = statisticUsers.get(0).getDate();
+                    recordDataArrayListFinal.add(new RecyclerViewRowRecordData(0, prevDate));
+                    int num = 0;
+                    for(RecyclerViewRowRecordData r : recordDataArrayListTmp) {
+                        if(prevDate == r.getDate()){
+                            recordDataArrayListFinal.get(recordDataArrayListFinal.size() - 1).setValue(recordDataArrayListFinal.get(recordDataArrayListFinal.size() - 1).getValue() + r.getValue());
+                            num++;
+                            continue;
+                        }
+                        recordDataArrayListFinal.get(recordDataArrayListFinal.size() - 1).setValue(recordDataArrayListFinal.get(recordDataArrayListFinal.size() - 1).getValue() / num);
+                        num = 1;
+                        recordDataArrayListFinal.add(new RecyclerViewRowRecordData(r.getValue(), r.getDate()));
+                        prevDate = r.getDate();
+                    }
+                    recordDataArrayListFinal.get(recordDataArrayListFinal.size() - 1).setValue(recordDataArrayListFinal.get(recordDataArrayListFinal.size() - 1).getValue() / num);
                 }
                 if((value - lastValue) > 0){
-                    recyclerViewRowDataArrayList.add(new RecyclerViewRowData(c.getName(), value, R.drawable.red_triangle, recordDataArrayList));
+                    recyclerViewRowDataArrayList.add(new RecyclerViewRowData(c.getName(), value, R.drawable.red_triangle, recordDataArrayListFinal));
                 }
                 else if((value - lastValue) < 0){
-                    recyclerViewRowDataArrayList.add(new RecyclerViewRowData(c.getName(), value, R.drawable.green_triangle, recordDataArrayList));
+                    recyclerViewRowDataArrayList.add(new RecyclerViewRowData(c.getName(), value, R.drawable.green_triangle, recordDataArrayListFinal));
                 }
                 else{
-                    recyclerViewRowDataArrayList.add(new RecyclerViewRowData(c.getName(), value, R.drawable.orange_rectangle, recordDataArrayList));
+                    recyclerViewRowDataArrayList.add(new RecyclerViewRowData(c.getName(), value, R.drawable.orange_rectangle, recordDataArrayListFinal));
                 }
             }
         }
--- app/src/main/java/com/example/mm/homeActivity/statisticFragment/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/mm/homeActivity/statisticFragment/RecyclerViewAdapter.java
@@ -1,7 +1,6 @@
 package com.example.mm.homeActivity.statisticFragment;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,10 +13,20 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mm.R;
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.DataSet;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+import com.google.android.material.slider.LabelFormatter;
 
 import java.util.ArrayList;
 
@@ -44,25 +53,26 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         Drawable res = context.getResources().getDrawable(recyclerViewRowDataArrayList.get(position).getImagesTrend());
         holder.recyclerViewTrendIcon.setImageDrawable(res);
 
-        ArrayList<Entry> entries = new ArrayList<>();
+        ArrayList<BarEntry> entries = new ArrayList<>();
         int index = 0;
         for (RecyclerViewRowRecordData i : recyclerViewRowDataArrayList.get(position).getRecordDataArrayList()) {
-            entries.add(new Entry(index, i.getValue()));
+            entries.add(new BarEntry(index, i.getValue()));
             index++;
         }
-        LineDataSet dataSet = new LineDataSet(entries, "Data");
+
+        BarDataSet dataSet = new BarDataSet(entries, "Data");
 
         dataSet.setColors(context.getResources().getColor(R.color.cyan));
         dataSet.setValueTextColor(context.getResources().getColor(R.color.black));
         dataSet.setValueTextSize(10f);
-        dataSet.setLineWidth(3);
 
-        LineData lineData = new LineData(dataSet);
+        BarData barData = new BarData(dataSet);
 
-        holder.recyclerviewRowLineChart.getDescription().setEnabled(false);
-        holder.recyclerviewRowLineChart.getLegend().setEnabled(false);
-        holder.recyclerviewRowLineChart.setData(lineData);
-        holder.recyclerviewRowLineChart.invalidate();
+        holder.recyclerviewRowBarChart.getDescription().setEnabled(false);
+        holder.recyclerviewRowBarChart.getLegend().setEnabled(false);
+        holder.recyclerviewRowBarChart.setData(barData);
+        holder.recyclerviewRowBarChart.getXAxis().setEnabled(false);
+        holder.recyclerviewRowBarChart.invalidate();
 
         if(recyclerViewRowDataArrayList.get(position).getRecordDataArrayList().isEmpty()){
             holder.warning.setTextColor(context.getResources().getColor(R.color.red));
@@ -88,27 +98,36 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         ImageView recyclerViewTrendIcon;
         ConstraintLayout recyclerviewRowMainLayout;
         ConstraintLayout recyclerviewRowExpandableLayout;
-        LineChart recyclerviewRowLineChart;
+        BarChart recyclerviewRowBarChart;
         public ViewHolder(View view){
             super(view);
-            course = view.findViewById(R.id.RecyclerviewRowCourse);
-            value = view.findViewById(R.id.RecyclerviewRowValue);
-            warning = view.findViewById(R.id.RecyclerviewRowWarning);
-            recyclerViewTrendIcon = view.findViewById(R.id.RecyclerViewTrendIcon);
-            recyclerviewRowMainLayout = view.findViewById(R.id.RecyclerviewRowMainLayout);
-            recyclerviewRowExpandableLayout = view.findViewById(R.id.RecyclerviewRowExpandableLayout);
-            recyclerviewRowLineChart = view.findViewById(R.id.RecyclerviewRowLineChart);
+            course = (TextView) view.findViewById(R.id.RecyclerviewRowCourse);
+            value = (TextView) view.findViewById(R.id.RecyclerviewRowValue);
+            warning = (TextView) view.findViewById(R.id.RecyclerviewRowWarning);
+            recyclerViewTrendIcon = (ImageView) view.findViewById(R.id.RecyclerViewTrendIcon);
+            recyclerviewRowMainLayout = (ConstraintLayout) view.findViewById(R.id.RecyclerviewRowMainLayout);
+            recyclerviewRowExpandableLayout = (ConstraintLayout) view.findViewById(R.id.RecyclerviewRowExpandableLayout);
+            recyclerviewRowBarChart = (BarChart) view.findViewById(R.id.RecyclerviewRowBarChart);
 
             recyclerviewRowMainLayout.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    /* "getAbsoluteAdapterPosition()" returns the Adapter position of the item represented
-                     * by this ViewHolder */
-                    RecyclerViewRowData recyclerViewRowData = recyclerViewRowDataArrayList.get(getAbsoluteAdapterPosition());
-                    recyclerViewRowData.setExpanded(!recyclerViewRowData.isExpanded());
-                    /* "notifyItemChanged()" will call "onBindViewHolder()" method so the Layout will be
-                     * updated. */
-                    notifyItemChanged(getAbsoluteAdapterPosition());
+                    if(recyclerViewRowDataArrayList.get(getAbsoluteAdapterPosition()).isExpanded == false) {
+                        for (int i = 0; i < (recyclerViewRowDataArrayList.size()); i++) {
+                            /* "getAbsoluteAdapterPosition()" returns the Adapter position of the item represented
+                             * by this ViewHolder */
+                            RecyclerViewRowData recyclerViewRowData = recyclerViewRowDataArrayList.get(i);
+                            recyclerViewRowData.setExpanded(getAbsoluteAdapterPosition() == i ? (!recyclerViewRowData.isExpanded()) : false);
+                            /* "notifyItemChanged()" will call "onBindViewHolder()" method so the Layout will be
+                             * updated. */
+                            notifyItemChanged(i);
+                        }
+                    }
+                    else{
+                        RecyclerViewRowData recyclerViewRowData = recyclerViewRowDataArrayList.get(getAbsoluteAdapterPosition());
+                        recyclerViewRowData.setExpanded(false);
+                        notifyItemChanged(getAbsoluteAdapterPosition());
+                    }
                 }
             });
         }
