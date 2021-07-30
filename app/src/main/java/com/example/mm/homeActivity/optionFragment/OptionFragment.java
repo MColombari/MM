f3f0f555150eee2dff9d5dff6a7f0b8dec5715cd
mattiacolombari <mattia.colombari00@gmail.com>
2021-07-30 20:58:32 +0200

    Add static structure for Backup in OptionFragment.java, modified TextView used as button with underline text ("Update Information" doesn't work), add Question Mark icon with popup window (Generic popup window .xlm structure added) and text in OptionFragment.java and StatisticFragment.java and modified Add/Update User Information popup window.

--- app/src/main/java/com/example/mm/homeActivity/optionFragment/OptionFragment.java
+++ app/src/main/java/com/example/mm/homeActivity/optionFragment/OptionFragment.java
@@ -10,6 +10,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
@@ -32,8 +33,13 @@ public class OptionFragment extends Fragment implements View.OnClickListener {
     TextView optionFragmentPlainTextMatr;
     TextView optionFragmentTextButton;
 
+    ImageView optionFragmentQuestionMarkUserInformation;
+    ImageView optionFragmentQuestionMarkBackup;
+    ImageView optionFragmentQuestionMarkManageCourse;
+
     View view;
-    PopupWindow popupWindow;
+    PopupWindow UserInformationPopupWindow;
+    PopupWindow questionMarkPopupWindow;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -52,7 +58,14 @@ public class OptionFragment extends Fragment implements View.OnClickListener {
         textViewMatr = (TextView) view.findViewById(R.id.TextViewMatr);
         textViewUpdateInfo = (TextView) view.findViewById(R.id.TexrViewUpdateUserInfo);
 
+        optionFragmentQuestionMarkUserInformation = (ImageView) view.findViewById(R.id.Option_Fragment_Question_Mark_User_Information);
+        optionFragmentQuestionMarkBackup = (ImageView) view.findViewById(R.id.Option_Fragment_Question_Mark_Backup);
+        optionFragmentQuestionMarkManageCourse = (ImageView) view.findViewById(R.id.Option_Fragment_Question_Mark_Manage_Course);
+
         textViewUpdateInfo.setOnClickListener(this);
+        optionFragmentQuestionMarkUserInformation.setOnClickListener(this);
+        optionFragmentQuestionMarkBackup.setOnClickListener(this);
+        optionFragmentQuestionMarkManageCourse.setOnClickListener(this);
 
         Thread t = new Thread(new getUserInformation(getContext(), this));
         t.start();
@@ -82,35 +95,94 @@ public class OptionFragment extends Fragment implements View.OnClickListener {
             LayoutInflater layoutInflater = (LayoutInflater) getContext().getSystemService(LAYOUT_INFLATER_SERVICE);
             View popupView = layoutInflater.inflate(R.layout.fragment_option_popup_window, null);
 
-            popupWindow = new PopupWindow(popupView, 900,1200, true);
-
+            UserInformationPopupWindow = new PopupWindow(popupView, ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT, true);
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                popupWindow.setElevation(20);
+                UserInformationPopupWindow.setElevation(20);
             }
-
+            UserInformationPopupWindow.setAnimationStyle(R.style.AnimationGenericPopupWindow);
+            UserInformationPopupWindow.update();
             /* "v" is used as a parent view to get the View.getWindowToken() token from. */
-            popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+            UserInformationPopupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
 
             optionFragmentPlainTextName = (TextView) popupView.findViewById(R.id.OptionFragmentPlainTextName);
             optionFragmentPlainTextSurname = (TextView) popupView.findViewById(R.id.OptionFragmentPlainTextSurname);
             optionFragmentPlainTextEmail = (TextView) popupView.findViewById(R.id.OptionFragmentPlainTextEmail);
             optionFragmentPlainTextMatr = (TextView) popupView.findViewById(R.id.OptionFragmentPlainTextMatr);
-            optionFragmentTextButton = (TextView) popupView.findViewById(R.id.OptionFragmentTextButton);
+            optionFragmentTextButton = (TextView) popupView.findViewById(R.id.OptionFragmentPopupWindowAddUpdateUserInfo);
             optionFragmentTextButton.setOnClickListener(this);
         }
-        else if (v.getId() == R.id.OptionFragmentTextButton){
+        else if (v.getId() == R.id.OptionFragmentPopupWindowAddUpdateUserInfo){
+            /* Set user information */
             try {
                 String name = optionFragmentPlainTextName.getText().toString();
                 String surname = optionFragmentPlainTextSurname.getText().toString();
                 String email = optionFragmentPlainTextEmail.getText().toString();
                 int matr = Integer.parseInt(optionFragmentPlainTextMatr.getText().toString());
 
-                Thread t = new Thread(new setUserInformation(getContext(), popupWindow, this, name, surname, email, matr));
+                Thread t = new Thread(new setUserInformation(getContext(), UserInformationPopupWindow, this, name, surname, email, matr));
                 t.start();
             }
             catch (NumberFormatException e){
                 optionFragmentTextButton.setText("Error, check matr, and try again.");
                 optionFragmentTextButton.setTextColor(getResources().getColor(R.color.red));
+            }
+        }
+        else if (v.getId() == R.id.Option_Fragment_Question_Mark_User_Information ||
+                v.getId() == R.id.Option_Fragment_Question_Mark_Backup ||
+                v.getId() == R.id.Option_Fragment_Question_Mark_Manage_Course){
+            /* Show information about  */
+            LayoutInflater layoutInflater = (LayoutInflater) getContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+            View popupView = layoutInflater.inflate(R.layout.generic_popup_window, null);
+
+            questionMarkPopupWindow = new PopupWindow(popupView, ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT, true);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                questionMarkPopupWindow.setElevation(20);
+            }
+            questionMarkPopupWindow.setAnimationStyle(R.style.AnimationGenericPopupWindow);
+            questionMarkPopupWindow.update();
+            /* "v" is used as a parent view to get the View.getWindowToken() token from. */
+            questionMarkPopupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+
+            TextView text = (TextView) popupView.findViewById(R.id.Generic_Popup_Window_Text);
+            TextView title = (TextView) popupView.findViewById(R.id.Generic_Popup_Window_Title);
+            TextView button = (TextView) popupView.findViewById(R.id.Generic_Popup_Window_Ok);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    questionMarkPopupWindow.dismiss();
+                }
+            });
+
+            switch (v.getId()){
+                case R.id.Option_Fragment_Question_Mark_User_Information:
+                    title.setText("More about user information.");
+                    text.setText(   "User information are information need by the app for download or" +
+                                    " load progress backup form the database, all this data" +
+                                    " could be see by authorized people only (to check the information" +
+                                    " of user, that need to be students of university of UNIMORE.)" +
+                                    " if you aren't a student of UNIMORE you could louse all your progress.");
+                    break;
+                case R.id.Option_Fragment_Question_Mark_Backup:
+                    title.setText("More about backup.");
+                    text.setText(   "Backup is essential to keep your progress save, and being able to " +
+                                    "change device without lose some important feature of this app " +
+                                    "such as the algorithms to sort question as you needed, or " +
+                                    "the statistic showed with graph to monitor your progress.\n" +
+                                    "Downloading the backup will override your local progress " +
+                                    "and uploading your backup will override your cloud backup.\n" +
+                                    "All the information store in the cloud are not seen by any " +
+                                    "professor or used to set school grades, but they can be used " +
+                                    "to make generic (non individual) graph for the professor or " +
+                                    "anyone else.");
+                    break;
+                case R.id.Option_Fragment_Question_Mark_Manage_Course:
+                    title.setText("More about manage courses.");
+                    text.setText(   "Manage your courses, such as remove courses, add courses or erase " +
+                                    "information about courses.\n" +
+                                    "Deleting a course will automatically delete all your statistic " +
+                                    "about it.");
+                    break;
             }
         }
     }
--- app/src/main/java/com/example/mm/homeActivity/statisticFragment/StatisticFragment.java
+++ app/src/main/java/com/example/mm/homeActivity/statisticFragment/StatisticFragment.java
@@ -1,14 +1,18 @@
 package com.example.mm.homeActivity.statisticFragment;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.example.mm.R;
@@ -25,9 +29,11 @@ import java.util.ArrayList;
 
 import com.example.mm.homeActivity.localDatabaseInteraction.getCourseStatistic;
 
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
 /* https://www.anychart.com/technical-integrations/samples/android-charts/ */
 
-public class StatisticFragment extends Fragment {
+public class StatisticFragment extends Fragment implements View.OnClickListener {
     TextView status;
     TextView statisticMoreText;
     RadarChart radarChart;
@@ -46,7 +52,10 @@ public class StatisticFragment extends Fragment {
         radarChart = view.findViewById(R.id.StatisticGraph);
         statisticMoreText = view.findViewById(R.id.StatisticMoreText);
 
+        ImageView questionMark = (ImageView) view.findViewById(R.id.Statistic_Fragment_Question_Mark);
+
         statisticMoreText.setOnClickListener((Home) getActivity());
+        questionMark.setOnClickListener(this);
 
         Thread t = new Thread(new getCourseStatistic(getContext(), this));
         t.start();
@@ -92,5 +101,43 @@ public class StatisticFragment extends Fragment {
         yAxis.setLabelCount(10, false);
 
         radarChart.invalidate();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.Statistic_Fragment_Question_Mark){
+            /* Show information about  */
+            LayoutInflater layoutInflater = (LayoutInflater) getContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+            View popupView = layoutInflater.inflate(R.layout.generic_popup_window, null);
+
+            PopupWindow popupWindow = new PopupWindow(popupView, ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT, true);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                popupWindow.setElevation(20);
+            }
+            popupWindow.setAnimationStyle(R.style.AnimationGenericPopupWindow);
+            popupWindow.update();
+            /* "v" is used as a parent view to get the View.getWindowToken() token from. */
+            popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+
+            TextView text = (TextView) popupView.findViewById(R.id.Generic_Popup_Window_Text);
+            TextView title = (TextView) popupView.findViewById(R.id.Generic_Popup_Window_Title);
+            TextView button = (TextView) popupView.findViewById(R.id.Generic_Popup_Window_Ok);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    popupWindow.dismiss();
+                }
+            });
+
+            title.setText("More about user statistics.");
+            text.setText(   "The radar graph shown rapresents the average points per courses, the " +
+                            "points range is [0-100].\n" +
+                            "Click 'More' to see more information per course.\n" +
+                            "If no data is shown it means that there aren't any record about any " +
+                            "question the used has answered, this could happen when all the courses " +
+                            "were deleted, or because you never answer any question.");
+
+        }
     }
 }
